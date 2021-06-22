7071bae69e74ea0005b9c11f9831b7bcaa63c241
brunobandelli <brunobandelli@gmail.com>
2021-06-21 21:57:52 -0300

    FORMULARIO E VALIDAÇÃO
    FORMULARIO E VALIDAÇÃO UTLIZANDO EVENTOS E VMODEL

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
+    <h3>Cadastro: </h3>
+    <small id="nomeErro" v-show="deuErro">O nome é inválido, tente novamente!</small><br>
+    <input type="text" placeholder="nome" v-model="nomeField"> <br>
+    <input type="email" placeholder="email" v-model="emailField"> <br>
+    <input type="number" placeholder="idade" v-model="idadeField"> <br>
+    <button @click="cadastrarUsuario">Cadastrar</button>
+    <hr>
     <!-- <h1>Guia Clientes</h1> -->
     <div v-for="(cliente,index) in clientes" :key="cliente.id">
       <h4>{{index}}</h4>
       <Cliente :cliente="cliente"/>
-      <hr>
+      <!-- <hr>
       <h4>Edição: </h4>
       <input type="text" v-model="cliente.nome">
-      <input type="text" v-model="cliente.email">
+      <input type="text" v-model="cliente.email"> -->
     </div>
     <!-- <hr>
       <h4>Lista de produtos!</h4>
@@ -25,12 +32,16 @@ export default {
   name: 'App',
   data(){
     return {
-      nomeDoBruno: "Bruno Ap. Bandelli V.",
-      clienteBruno: {
-        nome: "Bruno Bandelli",
-        email: "Bruno@bandelli.com",
-        idade: 26
-      },
+      deuErro: false,
+      nomeField: "",
+      emailField: "",
+      idadeField: 0,
+      // nomeDoBruno: "Bruno Ap. Bandelli V.",
+      // clienteBruno: {
+      //   nome: "Bruno Bandelli",
+      //   email: "Bruno@bandelli.com",
+      //   idade: 26
+      // },
       clientes: [
         {
         id: 7,
@@ -44,28 +55,45 @@ export default {
         email: "Bandelli@bandelli.com",
         idade: 21
         },
-        {
-        id: 27,
-        nome: "Brulli",
-        email: "Brulli@bandelli.com",
-        idade: 18
-        },
-        {
-        id: 77,
-        nome: "Brunelli",
-        email: "Brunelli@bandelli.com",
-        idade: 17
-        }
+        // {
+        // id: 27,
+        // nome: "Brulli",
+        // email: "Brulli@bandelli.com",
+        // idade: 18
+        // },
+        // {
+        // id: 77,
+        // nome: "Brunelli",
+        // email: "Brunelli@bandelli.com",
+        // idade: 17
+        // }
       ]
     }
   },
   components: {
     Cliente,
     // Produto
+  },
+  methods: {
+    cadastrarUsuario: function(){
+      if(this.nomeField == "" || this.nomeField == " " || this.nomeField.length < 3){
+        this.deuErro = true;
+      }else{
+      this.clientes.push({nome: this.nomeField, email: this.emailField, idade: this.idadeField, id: Date.now()});
+      this.nomeField = "";
+      this.emailField = "";
+      this.idadeField = "0";
+        this.deuErro = false;
+      }
+    }
   }
 }
 </script>
 
 <style>
+
+  #nomeErro{
+    color: red
+  }
 
 </style>
